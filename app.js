747600c9c65219a4e16e554522cac04babbf1268
Ervin <hamoydoerik@gmail.com>
2021-03-10 15:08:40 +0200

    Connected to the server

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
 
 const app = express();
 
-const PORT = config.get('port') || 5000;
+const PORT = config.get('port') || 3000;
 
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+async function start() {
+   try {
+      await mongoose.connect(config.get('mongoURI'), {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         useCreateIndex: true,
+         useFindAndModify: false
+      });
+
+      app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+   } catch (error) {
+      console.log('Server error', error.message);
+      process.exit(1);
+   }
+}
+
+start();
