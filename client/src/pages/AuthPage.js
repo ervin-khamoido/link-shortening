7de5545da41e6c7dbeca9a3b4968e3175bb4d849
Ervin <hamoydoerik@gmail.com>
2021-03-13 15:29:08 +0200

    Added visual component for AuthPage

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,7 +1,59 @@
+import { useState } from "react"
+
 export const  AuthPage = () => {
+   const [form, setForm] = useState({
+      email: '',
+      password: ''
+   });
+
+   const changeHandler = event => {
+      setForm({
+         ...form, 
+         [event.target.name]: event.target.value
+      });
+   };
+
    return (
-      <div>
-         <h1>Auth Page</h1>
+      <div className="row">
+         <div className="col s6 offset-s3">
+            <h1>Shorten the link</h1>
+
+            <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+               <span className="card-title">Authorization</span>
+
+               <div>
+                  <div className="input-field">
+                     <input 
+                        placeholder="Enter email"
+                        id="email"
+                        type="text"
+                        class="validate"
+                        name="email"
+                        onChange={changeHandler}
+                     />
+                     <label htmlFor="email">Email</label>
+                  </div>
+
+                  <div className="input-field">
+                     <input 
+                        placeholder="Enter password"
+                        id="password"
+                        type="text"
+                        class="validate"
+                        name="password"
+                        onChange={changeHandler}
+                     />
+                     <label htmlFor="password">Password</label>
+                  </div>
+               </div>
+            </div>
+            <div className="card-action">
+               <button className="auth-page-btn btn yellow darken-4">LogIn</button>
+               <button className="auth-page-btn btn grey lighten-1 black-text">SignUp</button>
+            </div>
+            </div>
+         </div>
       </div>
    )
 }
