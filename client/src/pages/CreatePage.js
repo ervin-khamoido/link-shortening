6177e028b8e6f926b2f8c2b322259336ad6809fb
Ervin <hamoydoerik@gmail.com>
2021-03-16 10:04:25 +0200

    Created CreatePage functionality

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,7 +1,44 @@
+import { useContext, useEffect, useState } from "react"
+import { useHistory } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { useHttp } from "../hooks/http.hook";
+
 export const CreatePage = () => {
+   const history = useHistory();
+   const auth = useContext(AuthContext);
+   const {request} = useHttp();
+   const [link, setLink] = useState('');
+
+   useEffect(() => {
+      window.M.updateTextFields();
+   }, [])
+
+   const pressHandler = async event => {
+      if (event.key === 'Enter') {
+         try {
+            const data = await request('/api/link/generate', 'POST', {from: link}, {Authorization: `Bearer ${auth.token}`});
+            history.push(`/detail/${data.link._id}`);
+            console.log(data);
+         } catch (error) {}
+      }
+   };
+
    return (
-      <div>
-         <h1>Create Page</h1>
+      <div className="row">
+         <div className="col s6 offset-s2" style={{paddingTop: '2rem'}}>
+            <div className="input-field">
+               <input 
+                  placeholder="Past a link"
+                  id="link"
+                  type="text"
+                  className="validate"
+                  value={link}
+                  onChange={e => setLink(e.target.value)}
+                  onKeyPress={pressHandler}
+               />
+               <label htmlFor="link">Enter the link</label>
+            </div>
+         </div>
       </div>
    )
 }
