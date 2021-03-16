53ccb42259efa92d7d455c286f69e6f67e36f17f
Ervin <hamoydoerik@gmail.com>
2021-03-16 20:51:07 +0200

    Refinement of business logic on the server

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes.js'));
 app.use('/api/link', require('./routes/link.routes.js'));
+app.use('/t', require('./routes/redirect.routes'));
 
 const PORT = config.get('port') || 5000;
 
